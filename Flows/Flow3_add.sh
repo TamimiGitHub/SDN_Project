7294c13c46263f4bc9938a575568a663f4c3b5b2
Tamimi <ahmadtamimi.mail@gmail.com>
2016-03-15 22:25:48 -0400

    added user customization

--- Flows/Flow3_add.sh
+++ Flows/Flow3_add.sh
@@ -1,5 +1,4 @@
-# COMMENT INFO
+# This flow Rerouts all traffic going to S2 network to host 6
 
 echo "This flow Rerouts all traffic going to S2 network to host 6 "
-
 curl -X PUT -d @Flow3 -H "Content-Type: application/xml" -H "Accept: application/xml" --user admin:admin http://localhost:8181/restconf/config/opendaylight-inventory:nodes/node/openflow:2/table/0/flow/5
---
+++ Flows/README.txt
@@ -0,0 +1,6 @@
+In this folder; user will ONLY invoke one of the files
+
+Flow1_add.sh {with two parameters} 
+Flow2_add.sh {with one parameter}
+
+Note: All files in this folder should be present together
